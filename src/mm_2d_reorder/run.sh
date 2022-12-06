bd0ab5b4596bf2357e86fd477be7b7447e0ba5d1
canesche <michael.canesche@gmail.com>
2022-12-06 14:35:34 -0300

    update script to execute experiment

--- src/mm_2d_reorder/run.sh
+++ src/mm_2d_reorder/run.sh
@@ -12,5 +12,5 @@ for ((i = 0; i < ${#ORDER[@]}; i++)); do
     RESULTS="results/"$MACHINE"_"${ORDER[i]}".csv"
     echo "sum,A,B,t0,t1,t2,t3,t4,t5,t6,t7,t8,t9" > $RESULTS
     $COMPILER -O3 search_space_${ORDER[i]}.c matrix_lib.c -o bin/tile_${ORDER[i]}.out
-    ./bin/tile_${ORDER[i]}.out > $RESULTS
+    ./bin/tile_${ORDER[i]}.out >> $RESULTS
 done
