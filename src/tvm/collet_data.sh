52ae1eee8edeb6ca3902ff0274d3f0fb40e967b2
canesche <michael.canesche@gmail.com>
2023-03-08 08:51:25 -0300

    script updated

--- src/tvm/collet_data.sh
+++ src/tvm/collet_data.sh
@@ -25,13 +25,14 @@ TUNER=(
 echo "" > data.csv
 echo "" > time.csv
 for ((k = 0; k < ${#ARCH[@]}; k++)); do
-    echo ${ARCH[k]} >> time.csv
+    DATA="results/"${ARCH[k]}"_time.csv"
+    echo "" > $DATA
     for ((i = 0; i < ${#MODEL[@]}; i++)); do
-        echo ${MODEL[i]} >> time.csv
+        echo ${MODEL[i]} >> $DATA
         for ((j = 0; j < ${#TUNER[@]}; j++)); do
-            echo ${TUNER[j]} >> time.csv
+            echo ${TUNER[j]} >> $DATA
             #python3 script/get_best_config.py ${MODEL[i]} ${TUNER[j]} $ARCH >> data.csv
-            python3 script/get_time.py results/${MODEL[i]}/${ARCH[k]}/${TUNER[j]}"/summary.log" >> time.csv
+            python3 script/get_time.py results/${MODEL[i]}/${ARCH[k]}/${TUNER[j]}"/summary.log" >> $DATA
         done
     done
 done
