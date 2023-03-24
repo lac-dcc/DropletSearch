bb8a3677e8029b9d4ddcabfa9e900c9a3a1bcdd5
canesche <michael.canesche@gmail.com>
2023-03-24 20:48:41 -0300

    testing with droplet

--- docker/droplet_algorithm/droplet_turner.py
+++ docker/droplet_algorithm/droplet_turner.py
@@ -3,6 +3,7 @@
 import numpy as np
 from scipy import stats
 from .tuner import Tuner
+from .model_based_tuner import knob2point
 
 class DropletTuner(Tuner):
     """Tuner with droplet algorithm.
@@ -21,20 +22,19 @@ class DropletTuner(Tuner):
         self.space = task.config_space
         self.dims = []
 
-        for k, v in self.space.space_map.items():
+        for _, v in self.space.space_map.items():
             self.dims.append(len(v))
         
         #print(self.dims)
         # start position
         start_position =  [0] * len(self.dims) if start_position == None else start_position
-        self.next = [(self.convert_idx(start_position),start_position)]
-        self.visited = {}
-        self.visited[self.convert_idx(start_position)] = 1
-        self.batch, self.count, self.max_value, self.pvalue = 0, 0, 99999, pvalue
-        self.best_choice = (-1, [-1] * len(self.dims), [self.max_value])
+        self.next = [(knob2point(start_position, self.dims),start_position)]
+        self.visited = set([knob2point(start_position, self.dims)])
+        self.batch, self.count, self.pvalue = 16, 0, pvalue
+        self.best_choice = (-1, [-1] * len(self.dims), [99999])
         # number execution is important when the start position is not valid
-        self.number_execution = 1
-        self.total_number_execution = max(self.dims)//2
+        self.number_execution = 2
+        self.total_number_execution = max(self.dims)
     
     def number_to_bin(self, value, factor=1):
         """ convert a number to a binary vector.
@@ -42,60 +42,29 @@ class DropletTuner(Tuner):
         bin_format = str(0) * (len(self.dims) - len(bin(value)[2:])) + bin(value)[2:]
         return [int(i) * factor for i in bin_format]
 
-    def convert_idx(self, value):
-        index, exp = 0, 1
-        for i in range(0, len(value)):
-            index += (value[i] % self.dims[i]) * exp
-            exp *= self.dims[i]
-        return index
-
-    def new_search_space(self, factor=1):
+    def get_search_space(self, factor=1):
         '''Return the new search space 
         '''
-        #search_space = []
-        #for i in range(0,len(self.dims)): # [0,0,0] => [0,0,1], [0,1,0], [1,0,0]
-        #    search_space.append(self.number_to_bin(2**i, factor))
-        #    search_space.append(self.number_to_bin(2**i, -factor))
-        #return search_space
         search_space = []
-        for i in range(2**len(self.dims)-1,0,-1):
+        for i in range(1,2**len(self.dims)): # [0,0,0] => [0,0,1], [0,1,0], [1,0,0]. ...
             search_space.append(self.number_to_bin(i, factor))
-            if i > self.batch:
-                break
+            search_space.append(self.number_to_bin(i, -factor))
         return search_space
-    
-    def local_search_space(self, factor=1):
-        '''Return the new search space 
-        '''
-        search_space = []
-        for i in range(2**len(self.dims)-1,0,-1):
-            search_space.append(self.number_to_bin(i))
-            if i > self.batch:
-                break
-        return search_space
-    
-    def safe_value(self, data):
-        for d in data:
-            if d < 0:
-                return False
-        return True
 
     def next_positions(self, new_positions):
         next_set = []
         for p in new_positions:
-            new_p = [x + y for x, y in zip(p, self.best_choice[1])]
-            idx_p = self.convert_idx(new_p)
-            if self.safe_value(new_p) and idx_p not in self.visited.keys():
-                self.visited[idx_p] = 1
+            new_p = [(x + y) % self.dims[i] if x + y > 0 else 0 for i, (x, y) in enumerate(zip(p, self.best_choice[1]))]
+            idx_p = knob2point(new_p, self.dims)
+            if idx_p not in self.visited: # memoization
+                self.visited.add(idx_p)
                 next_set.append((idx_p,new_p))
         return next_set
 
     def p_value(self, elem_1 : np.array, elem_2 : np.array):
-        '''Return the p_value between two arrays
+        '''Return the p_value between two arrays, using Student's t-test
         '''
-        if len(elem_1) <= 1 or len(elem_2) <= 1: # Case that there is only one element
-            return True
-        return stats.ttest_ind(elem_1, elem_2).pvalue <= self.pvalue
+        return True if len(elem_1) <= 1 or len(elem_2) <= 1 else stats.ttest_ind(elem_1, elem_2).pvalue <= self.pvalue
 
     def next_batch(self, batch_size):
         '''Return the next batch 
@@ -108,9 +77,6 @@ class DropletTuner(Tuner):
             self.count += 1
             ret.append(self.space.get(self.next[i][0]))
         return ret
-
-    def update_next_element(self):
-        return self.next[self.count:-1]
     
     def update(self, inputs, results):
         '''Update the search space by measuring time 
@@ -126,18 +92,25 @@ class DropletTuner(Tuner):
         
         #print("exec:", self.number_execution)
         #print("best:", self.best_choice)
-        self.next = self.update_next_element()
-        while len(self.next) < self.batch and self.number_execution < self.total_number_execution:
-            if found_best_pos:
-                self.next += self.next_positions(self.local_search_space())
-                found_best_pos = False
-            else:
-                self.next += self.next_positions(self.new_search_space(self.number_execution))
+        self.next = self.next[self.count:-1]
+
+        if found_best_pos:
+            self.next += self.next_positions(self.get_search_space())
+
+            # Prediction the gradient descendent direction and fill the search space
+            while len(self.next) < self.batch:
+                self.next += self.next_positions(self.get_search_space(self.number_execution))
                 self.number_execution += 1
+
+        #while len(self.next) < self.batch and self.number_execution < self.total_number_execution:
+        #    if found_best_pos:
+        #        self.next += self.next_positions(self.local_search_space())
+        #        found_best_pos = False
+        #    else:
+        #        self.next += self.next_positions(self.new_search_space(self.number_execution))
+        #        self.number_execution += 1
+        #        break
         #print("next_elements:", self.next)
 
     def has_next(self):
         return len(self.next) > 0
-
-    def load_history(self, data_set, min_seed_records=500):
-        pass
