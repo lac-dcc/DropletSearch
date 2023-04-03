c06d7bb29e2b13006c14b274d3d593f6eee5103b
canesche <michael.canesche@gmail.com>
2023-04-03 19:03:50 -0300

    update

--- src/tvm/script/get_time.py
+++ src/tvm/script/get_time.py
@@ -2,6 +2,36 @@ import numpy as np
 import json
 import sys
 
+def process_each_time(log):
+
+    f = open(log, "r")
+    c = False
+    for l in f.readlines():
+        l = l.strip()
+        if "Time partial tuning:" in l:
+            v = l.split(":")[1].replace(" ","").split(",")
+            print("%s,%s,%s,%s" %(v[0],v[1],v[2],v[3]))
+        elif "mean (ms)" in l:
+            c = True
+        elif c:
+            l = l.split(" ")
+            #print("%s,%s,%.2f"%(l[0],l[-1],v_sum))
+            c = False
+    f.close() 
+
+def process_time_ansor(log):
+    layers = {}
+    f = open(log, "r")
+    for line in f.readlines():
+        data = json.loads(line)
+        if "ansor" in log:
+            print(data["i"][0])
+            break
+            #if data["i"][0] not in layers.keys():
+            #    print(data)
+            #    break
+            #layers[data["i"][0]]
+
 def process_time(log):
 
     f = open(log, "r")
@@ -28,4 +58,4 @@ if __name__ == "__main__":
 
     log = sys.argv[1]
     #print(log)
-    process_time(log)
+    process_time_ansor(log)
