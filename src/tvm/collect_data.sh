37158a914203903e10a6d63b7fed794fa5ba31c6
canesche <michael.canesche@gmail.com>
2023-04-18 14:33:06 -0300

    update script collect data

--- src/tvm/collect_data.sh
+++ src/tvm/collect_data.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 ARCH=(
-    "x86_AMD3900X"
-    "x86_inteli7-3770"
-    "cuda_rtx3080"
-    "cuda_gtx1650"
-    "arm_cortex-a7"
+    #"x86_AMD3900X"
+    #"x86_inteli7-3770"
+    #"cuda_rtx3080"
+    #"cuda_gtx1650"
+    #"arm_cortex-a7"
+    "arm_cortex-a72"
 )
 
 MODEL=(
@@ -36,11 +37,30 @@ for ((k = 0; k < ${#ARCH[@]}; k++)); do
             echo ${TUNER[j]} >> $DATA
             #python3 script/get_best_config.py ${MODEL[i]} ${TUNER[j]} $ARCH >> data.csv
             python3 script/get_time.py results/${ARCH[k]}/${MODEL[i]}/${TUNER[j]}"/summary.log" >> $DATA
-            python3 script/get_iter_times_partial.py results/${ARCH[k]}/${MODEL[i]}/${TUNER[j]} >> $DATA_ITER
+            #python3 script/get_iter_times_partial.py results/${ARCH[k]}/${MODEL[i]}/${TUNER[j]} >> $DATA_ITER
         done
         echo "" >> $DATA
         echo "" >> $DATA_ITER
     done
 done
 
+
+PVALUE=(
+    "0.01"
+    "0.05"
+    "0.10"
+    "0.25"
+    "1.00"
+)
+
+DATA="results/p_value/time.csv"
+echo "" > $DATA
+for ((l = 0; l < ${#PVALUE[@]}; l++)) do
+    echo "pvalue ${PVALUE[l]}" >> $DATA
+    for ((k = 0; k < ${#ARCH[@]}; k++)); do
+        for ((i = 0; i < ${#MODEL[@]}; i++)); do
+            python3 script/get_time.py results/p_value/x86_hokusai_p_${PVALUE[l]}/${MODEL[i]}"/droplet/summary.log" >> $DATA
+        done
+    done
+done
 
