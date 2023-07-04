41fad146183610f51ec7b487323df3368121718f
canesche <michael.canesche@gmail.com>
2023-07-04 09:28:57 -0300

    start implementation Droplet

--- thirdparty/Heron/tuner/droplet_tuner.py
+++ thirdparty/Heron/tuner/droplet_tuner.py
@@ -6,9 +6,9 @@ import numpy as np
 class DropletTuner(Tuner):
     def optimize(self, env, pop, stat, s_time):
         samples = [] + pop
-        print(samples)
         for i in range(self.config.iter_walks):
             population = self.constrained_random_sample(env, self.config.pop_num)
+            print(self.config.pop_num)
             samples = samples + population
             perfs = [x.predict for x in samples]
             stat.append([np.array(perfs).max(), time.time() - s_time])
