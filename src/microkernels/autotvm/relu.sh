1785c7cc7205b21d7d76eccd6830ea490bb4e04e
canesche <michael.canesche@gmail.com>
2023-07-10 12:41:59 -0300

    update

---
+++ src/microkernels/autotvm/relu.sh
@@ -0,0 +1,7 @@
+python3 src/relu_tuning.py relu 1024 1024 
+#python3 $1/injective_tuning.py relu 2048 1024 $2
+#python3 $1/injective_tuning.py relu 4096 1024 $2
+#python3 $1/injective_tuning.py relu 8192 1024 $2
+#python3 $1/injective_tuning.py relu 16384 1024 $2
+#python3 $1/injective_tuning.py relu 32768 1024 $2
+#python3 $1/injective_tuning.py relu 65536 1024 $2
