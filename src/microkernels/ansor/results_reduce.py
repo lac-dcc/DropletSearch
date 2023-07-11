21c8c52a79ce9e05959f9673ec35e75aededf181
canesche <michael.canesche@gmail.com>
2023-07-11 13:04:23 -0300

    update results

---
+++ src/microkernels/ansor/results_reduce.py
@@ -0,0 +1,58 @@
+----------------------------------------------------------------------
+------------------------------  [ Search ]
+----------------------------------------------------------------------
+Generate Sketches		#s: 2
+Sample Iter: 5	#Pop: 10	#Target: 50	fail_ct: 10230	Time elapsed: 1.97
+#Target has been reduced to 25 due to too many failures or duplications
+Sample Iter: 10	#Pop: 10	#Target: 25	fail_ct: 20470	Time elapsed: 3.93
+#Target has been reduced to 12 due to too many failures or duplications
+Sample Iter: 15	#Pop: 10	#Target: 12	fail_ct: 30710	Time elapsed: 5.88
+#Target has been reduced to 6 due to too many failures or duplications
+Sample Initial Population	#s: 10	fail_ct: 32758	Time elapsed: 6.27
+GA Iter: 0	Max score: 0.9115	Min score: 0.0016	#Pop: 10	#M+: 0	#M-: 0
+GA Iter: 4	Max score: 0.9975	Min score: 0.0016	#Pop: 70	#M+: 1390	#M-: 0
+EvolutionarySearch		#s: 70	Time elapsed: 1.46
+----------------------------------------------------------------------
+------------------------------  [ Measure ]
+----------------------------------------------------------------------
+----------------------------------------------------------------------
+------------------------------  [ Train cost model ]
+----------------------------------------------------------------------
+Time elapsed for training: 0.08 s
+----------------------------------------------------------------------
+------------------------------  [ Search ]
+----------------------------------------------------------------------
+Sample Initial Population	#s: 10	fail_ct: 2038	Time elapsed: 0.40
+GA Iter: 0	Max score: 0.7511	Min score: 0.7511	#Pop: 1	#M+: 0	#M-: 0
+GA Iter: 4	Max score: 0.9425	Min score: 0.1515	#Pop: 6	#M+: 1396	#M-: 0
+EvolutionarySearch		#s: 6	Time elapsed: 1.47
+----------------------------------------------------------------------
+------------------------------  [ Measure ]
+----------------------------------------------------------------------
+----------------------------------------------------------------------
+------------------------------  [ Train cost model ]
+----------------------------------------------------------------------
+Time elapsed for training: 0.06 s
+----------------------------------------------------------------------
+------------------------------  [ Search ]
+----------------------------------------------------------------------
+Sample Initial Population	#s: 10	fail_ct: 2038	Time elapsed: 0.40
+GA Iter: 0	Max score: N/A	Min score: N/A	#Pop: 0	#M+: 0	#M-: 0
+GA Iter: 4	Max score: N/A	Min score: N/A	#Pop: 0	#M+: 1397	#M-: 0
+EvolutionarySearch		#s: 0	Time elapsed: 1.48
+----------------------------------------------------------------------
+------------------------------  [ Search ]
+----------------------------------------------------------------------
+Sample Initial Population	#s: 10	fail_ct: 2038	Time elapsed: 0.39
+GA Iter: 0	Max score: N/A	Min score: N/A	#Pop: 0	#M+: 0	#M-: 0
+GA Iter: 4	Max score: N/A	Min score: N/A	#Pop: 0	#M+: 1385	#M-: 0
+EvolutionarySearch		#s: 0	Time elapsed: 1.45
+It seems all candidates in the search space have been measured.
+----------------------------------------------------------------------
+------------------------------  [ Done ]
+----------------------------------------------------------------------
+(128, 512, 1024) axis: 2
+Get devices for measurement successfully!
+best runtime:  0.31243299999999996
+compilation time: 161.24071110000006
+compilation time:  (inf, 0)
