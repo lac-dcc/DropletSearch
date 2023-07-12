bff7fd37e1575b2fa57ed57c1723417c3c34bdf9
canesche <michael.canesche@gmail.com>
2023-07-12 13:07:58 -0300

    microkernels

--- src/microkernels/tf/run.sh
+++ src/microkernels/tf/run.sh
@@ -4,4 +4,4 @@ python3 src/conv2d.py 128 128 28 28 128 3 3 1 1 SAME > results/results_conv2d.tx
 python3 src/depthwise.py 128 84 83 83 5 2 1 SAME 1000 > results/results_depthwise.txt
 python3 src/pool.py 128 168 83 83 1 2 VALID 1000 > results/results_pool.txt
 python3 src/reduce.py 0 1000 > results/results_reduce.txt
-python3 src/relu.py 1024 > results/results_relu.txt
+python3 src/relu.py 4096 > results/results_relu.txt
