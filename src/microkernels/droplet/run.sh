cf1609e476050331e08ca6c6131578a78602d20a
canesche <michael.canesche@gmail.com>
2023-12-08 15:59:10 -0300

    update folder and script

--- src/microkernels/droplet/run.sh
+++ src/microkernels/droplet/run.sh
@@ -1,8 +1,8 @@
 mkdir -p log
 mkdir -p results
-python3 src/matmul_tuning.py 16 1024 1024 log > results/results_matmul.txt
-python3 src/conv2d_tuning.py 128 128 28 28 128 3 3 1 1 SAME log > results/results_conv2d.txt
-python3 src/depthwise_tuning.py 128 84 83 83 5 5 2 1 SAME log > results/results_depthwise.txt
-python3 src/pooling_tuning.py avg 128 168 83 83 1 2 VALID log > results/results_pool.txt
-python3 src/reduction_tuning.py 128 512 1024 2 log > results/results_reduce.txt
-python3 src/relu_tuning.py relu 4096 4096 > results/results_relu.txt
+python3 src/matmul_tuning.py 16 1024 1024 log #> results/results_matmul.txt
+python3 src/conv2d_tuning.py 128 128 28 28 128 3 3 1 1 SAME log #> results/results_conv2d.txt
+python3 src/depthwise_tuning.py 128 84 83 83 5 5 2 1 SAME log #> results/results_depthwise.txt
+python3 src/pooling_tuning.py avg 128 168 83 83 1 2 VALID log #> results/results_pool.txt
+python3 src/reduction_tuning.py 128 512 1024 2 log #> results/results_reduce.txt
+python3 src/relu_tuning.py relu 4096 4096 #> results/results_relu.txt
